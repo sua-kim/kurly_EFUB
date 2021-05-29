e6156fa9a316f097feee6eec380e2d38506507e0
hyojin530 <dkakfl56@gmail.com>
2021-05-29 11:19:26 +0900

    first commit

---
+++ server/src/main/java/com/efub/kurlyclone/KurlycloneApplication.java
@@ -0,0 +1,13 @@
+package com.efub.kurlyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlycloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlycloneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/efub/kurlyclone/KurlycloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.kurlyclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurlycloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
