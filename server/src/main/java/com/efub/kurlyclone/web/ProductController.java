a07787298ed7382f7811cdad7c8f2b89b6ecbfc9
hyojin530 <dkakfl56@gmail.com>
2021-06-02 15:15:24 +0900

    product_crud

--- server/src/main/java/com/efub/kurlyclone/web/ProductController.java
+++ server/src/main/java/com/efub/kurlyclone/web/ProductController.java
@@ -1,4 +1,46 @@
 package com.efub.kurlyclone.web;
 
+import com.efub.kurlyclone.service.ProductService;
+import com.efub.kurlyclone.web.dto.ProductResponseDto;
+import com.efub.kurlyclone.web.dto.ProductSaveRequestDto;
+import com.efub.kurlyclone.web.dto.ProductUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RequiredArgsConstructor
+@CrossOrigin
+@RestController
+@RequestMapping("/api/product")
 public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/{product_id}")
+    public ProductResponseDto getProduct(@PathVariable Long product_id){
+        return productService.findById(product_id);
+    }
+
+    @PostMapping("/create")
+    public Long createProduct(
+            @RequestParam("file") MultipartFile file,
+            ProductSaveRequestDto productSaveRequestDto
+            ){
+        return productService.saveProduct(productSaveRequestDto, file);
+    }
+
+    @PostMapping("/update/{product_id}")
+    public Long updateProduct(
+            @PathVariable Long product_id,
+            @RequestBody ProductUpdateDto productUpdateDto
+            ){
+        return productService.updateProduct(product_id, productUpdateDto);
+    }
+
+    @GetMapping("/delete/{product_id}")
+    public String deleteProduct(@PathVariable Long product_id){
+        productService.deleteProduct(product_id);
+        return "OK";
+    }
 }
